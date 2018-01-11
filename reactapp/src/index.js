bcd1f84bbaa17c35d569e8e6ba8ae9d248972d30
张乐 <Oliver_web@163.com>
2018-01-11 22:26:58 +0800

    4/03

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+  import React from 'react'
 import ReactDom from 'react-dom'
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'

b456ff08dd339a86acd1ed344cc75fc74a15c5fc
张乐 <Oliver_web@163.com>
2018-01-11 16:16:07 +0800

    redux , react-router, 进行回顾完成

--- src/Auth.redux.js
+++ src/Auth.redux.js
@@ -1,4 +1,18 @@
+/*
+* 对数据的进行处理的都将利用redux进行处理
+* 与auth.js的@connecta 装饰器中的数据是的息息相关的,
+* connect中的数据以及用到的action
+* createStore(reducer) reducer的操作的函数的---逻辑处理
+* 以及dispatch(action)
+*用于存放数据供其他的木偶组件进行调用
+*
+* */
+
 import axios from  'axios'
+/*
+* 定义变量
+*
+* */
 const LOGIN = 'LOGIN';
 const LOGOUT = 'LOGOUT';
 const USER_DATA='USER_DATA';
@@ -9,6 +23,14 @@ const initState={
 	user:'李云龙',
 	age:20
 };
+
+/*
+* 这里是定义reducer操作的函数 createStore(reducer)
+*第一个参数:用到的数据的
+* 第二个参数是dispatch(action) 中的操作的库
+* switch 针对多种情况进行处理
+*
+* */
 export function auth(state = initState, action) {
 	console.log(state,action);
 	switch (action.type) {
@@ -23,7 +45,7 @@ export function auth(state = initState, action) {
 	}
 }
 
-//action  creator
+//action  creator  dispatch(action)
 export function getUserData(){
 	return dispatch=>{
 		axios.get('/data')

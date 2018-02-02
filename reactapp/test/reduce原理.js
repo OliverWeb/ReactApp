e7ba65fac774b820e34dc41427c9ccf89ab44ffb
张乐 <Oliver_web@163.com>
2018-02-02 10:45:09 +0800

    readux

--- reactapp/test/reduce原理.js
+++ reactapp/test/reduce原理.js
@@ -1,26 +1,29 @@
 import {createStore} from 'test/index.redux'
 //根据老的state和action生成新的state,代码的库的事件
 //createStore();函数库
-function couter(state=0,action) {
-	switch(action.type){
+function couter(state = 0, action) {
+	switch (action.type) {
 		case 'a':
-			return state+=6;
+			return state += 6;
 		default:
 			return 10;
 	}
 }
+
 /*新建store*/
-const  store=createStore(couter);       /*新建的store 库*/
+const store = createStore(couter);
+/*新建的store 库*/
 
 
 /*订阅的事件*/
 function listen() {
-	const current=store.getState();
+	const current = store.getState();
 	console.log(current);
 }
+
 //利用订阅事件进行监控,事件处理里中状态的变化
 store.subscribe(listen);
 //进行事件处理,派发事件,传递action;
-store.dispatch({type:'加机关枪'});   //dispatch(action);
+store.dispatch({type: '加机关枪'});   //dispatch(action);
 
 

2dd38cfaf37a4760f5205172411054a2e7c0de4f
张乐 <Oliver_web@163.com>
2018-02-06 17:43:59 +0800

    React原理

---
+++ 阶段二/src/demo.js
@@ -0,0 +1,24 @@
+import React from 'react'
+class Demo extends React.Component{
+	constructor(props){
+		super(props);
+		this.state={
+			num:1
+		};
+		this.handleClick=this.handleClick.bind(this);
+	}
+	handleClick(){
+		this.setState({
+			num:this.state.num+1
+		})
+	}
+	render(){
+		return(
+			<div>
+				<p>{num}</p>
+				<button onClick={this.handleClick}>Click</button>
+			</div>
+		)
+	}
+}
+export default Demo
